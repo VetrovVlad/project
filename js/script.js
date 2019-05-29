e180342b2c905084e6803c71bfe94561f722e878
VetrovVlad <49245458+VetrovVlad@users.noreply.github.com>
2019-05-29 19:08:00 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.services-About-desc-box').height($('.services-About-box img').height());
+
+});
+
+$('.Menu-header-TitlePage-box').on('click', function(){
+	$('.menu-icon').toggleClass('manu-icon-close');
+});
